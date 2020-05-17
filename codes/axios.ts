7e32a7f4ea260320e6b56d2a39195762ead4391f
Jacques Yakoub <jacques.yakoub@gmail.com>
2020-05-17 21:36:54 +0200

    code: simplify axios code (#28)
    * code: simplify axios code
    
    I hate if cascades ^^
    
    * wip: remove itemize restrictions

--- codes/axios.ts
+++ codes/axios.ts
@@ -3,24 +3,17 @@ try {
     commit('INIT', response);
   } catch (e) {
     const errorAxios = e as AxiosError;
-  
-    if (errorAxios.response) {
-      const status: number = errorAxios.response.status;
-  
-      if (status === 400) {
-        this.$displayError("Une erreur est survenue lors du chargement des exercices.", {statusCode: status});
-      } else if (status === 401) {
-        this.$displayError(`Vous devez vous connecter pour charger ces exercices.`, {statusCode: status});
-      } else if (status === 403) {
-        this.$displayError(`Vous n'êtes pas autorisé à effectuer cette action.`, {statusCode: status});
-      } else if (status === 500) {
-        this.$displayError(`Une erreur est survenue depuis nos serveurs, veuillez-nous en excuser.`, {statusCode: status});
-      } else {
-        this.$displayError("Une erreur est survenue lors du chargement des exercices.", {statusCode: status});
-      }
-    } else {
-        this.$displayError(`Une erreur est survenue lors du chargement des exercices.`, {statusCode: 400});
+    const status : number = (errorAxios.response) ? errorAxios.response.status : 400;
+    const errors : any = {
+      400: "Une erreur est survenue lors du chargement des exercices.",
+      401: `Vous devez vous connecter pour charger ces exercices.`,
+      403: `Vous n'êtes pas autorisé à effectuer cette action.`,
+      500: `Une erreur est survenue depuis nos serveurs, veuillez-nous en excuser.`,
     }
+
+    // fetch the message for this error
+    const message : string = (errors.hasOwnProperty(status)) ? errors[status] : errors[400];
+    this.$displayError(message, {statusCode: status});
   
     commit('RESET')
-  }
+}
